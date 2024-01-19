8b1c5ec105ae9d5f86348a54a13758ecc2d0d6b9
Daniel Kranich <Daniel@nusalaska.com>
2024-01-19 11:36:19 -0900

    Add heading and tab pages

--- popup.svelte
+++ popup.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import "~/style.css"
+
+  import * as Tabs from "~/lib/components/ui/tabs/index"
 </script>
 
-<div></div>
-
-<style>
-</style>
+<div>
+  <h1 class="text-center font-bold mb-2">Any Dice</h1>
+  <Tabs.Root value="dice" class="w-[400px]">
+    <Tabs.List class="grid w-full grid-cols-2">
+      <Tabs.Trigger value="dice">Select Dice</Tabs.Trigger>
+      <Tabs.Trigger value="options">Options</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="dice">This is for dice</Tabs.Content>
+    <Tabs.Content value="options">this is for options</Tabs.Content>
+  </Tabs.Root>
+</div>
