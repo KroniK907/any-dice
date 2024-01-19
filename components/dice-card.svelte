e7ac789bde06ad19e2d37f9692735787b41c83ee
Daniel Kranich <Daniel@nusalaska.com>
2024-01-19 11:35:18 -0900

    add shadcn-svelte components

---
+++ components/dice-card.svelte
@@ -0,0 +1,7 @@
+<script>
+  import * as Accordion from "~lib/components/ui/accordion/index"
+  import * as Avatar from "~lib/components/ui/avatar/index"
+  import * as Button from "~lib/components/ui/avatar/index"
+  import * as Card from "~lib/components/ui/card/index"
+  import * as Select from "~lib/components/ui/select/index"
+</script>
