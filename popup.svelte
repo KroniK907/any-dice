1697c41e2153f88c0f3b0edfc1eed345eb90cfba
Daniel Kranich <Daniel@nusalaska.com>
2024-01-18 16:04:01 -0900

    setup basic functionality to test concept

--- popup.svelte
+++ popup.svelte
@@ -1,39 +1,54 @@
 <script lang="ts">
-    import { Storage } from '@plasmohq/storage';
+  import { Storage } from "@plasmohq/storage"
 
-    const storage = new Storage({area: 'local'});
+  const storage = new Storage({ area: "local" })
 
-    async function blackSet() {
-        await storage.set('selectedSet', 'diceSet-00101');
-    }
-    async function glacialSet() {
-        await storage.set('selectedSet', 'diceSet-00701')
-    }
+  async function blackSet() {
+    await storage.set("selectedSet", "diceSet-00101")
+    reloadCharacterSheets()
+  }
+  async function glacialSet() {
+    await storage.set("selectedSet", "diceSet-00701")
+    reloadCharacterSheets()
+  }
+
+  function reloadCharacterSheets() {
+    chrome.tabs.query(
+      { url: "https://www.dndbeyond.com/characters/*" },
+      (tabs) => {
+        if (tabs) {
+          for (let tab of tabs) {
+            chrome.tabs.reload(tab.id)
+          }
+        }
+      }
+    )
+  }
 </script>
 
-<style>
-    .container {
-        min-width: 470px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 47px;
-    }
-    .action {
-        color: #470;
-        font-weight: bold;
-    }
-    .text-center {
-        text-align: center;
-    }
-</style>
-
 <div>
-    <h2 class="text-center">
-      Welcome to your <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
-    </h2>
-    <div class="container">
-        <button on:click={blackSet}>Black Dice</button>
-        <button on:click={glacialSet}>Glacial Dice</button>
-    </div>
+  <h2 class="text-center">
+    Welcome to your <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
+  </h2>
+  <div class="container">
+    <button on:click={blackSet}>Black Dice</button>
+    <button on:click={glacialSet}>Glacial Dice</button>
+  </div>
 </div>
+
+<style>
+  .container {
+    min-width: 470px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 47px;
+  }
+  .action {
+    color: #470;
+    font-weight: bold;
+  }
+  .text-center {
+    text-align: center;
+  }
+</style>
